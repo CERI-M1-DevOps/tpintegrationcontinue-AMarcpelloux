708a8c097596e5644969e169b57d8713010cc599
Marc Pelloux <marc.pelloux@alumni.univ-avignon.fr>
2024-11-08 16:30:22 +0100

    issus fix 4

--- src/test/java/liste/ListeSimpleTest.java
+++ src/test/java/liste/ListeSimpleTest.java
@@ -59,7 +59,7 @@ class ListeSimpleTest {
         listeATester.ajout(2);
         listeATester.ajout(3);
         listeATester.modifiePremier(2, 4);
-        assertEquals( "ListeSimple(Noeud(3), Noeud(4), Noeud(1))",listeATester.toString(),);
+        assertEquals( "ListeSimple(Noeud(3), Noeud(4), Noeud(1))",listeATester.toString());
         assertEquals(4, listeATester.tete.getSuivant().getElement());
     }
 
@@ -109,7 +109,7 @@ class ListeSimpleTest {
         listeATester.ajout(3);
         listeATester.ajout(4);
         listeATester.supprimePremier(1);
-        assertEquals(, "ListeSimple(Noeud(4), Noeud(3), Noeud(2))",listeATester.toString());
+        assertEquals( "ListeSimple(Noeud(4), Noeud(3), Noeud(2))",listeATester.toString());
         assertEquals(3, listeATester.getSize());
     }
 
